8aa1bf262873658e7ee5c45683269828f153c65e
bgroveben <bgroveben@gmail.com>
2016-07-26 02:58:11 -0700

    Section 5.5 -- Dictionaries

--- 05_data_structures.py
+++ 05_data_structures.py
@@ -241,5 +241,25 @@ print(a)
 
 
 # 5.5. Dictionaries
-
+# aka "associative memories", "associative arrays", "hashes".
+# Dictionaries are indexed by keys, which can be any immutable type.
+# Think of a dictionary as an unordered set of key:value pairs that require the keys to be unique.
+# You can create an empty dictionary with a pair of curly braces {}.
+tel = {'jack': 4098, 'sape': 4139}
+tel['guido'] = 4127
+print(tel)
+print(tel['jack'])
+del tel['sape']
+tel['irv'] = 4127
+print(tel)
+print(list(tel.keys()))
+print(sorted(tel.keys()))
+print('guido' in tel)
+print('jack' not in tel)
+# The dict() constructor builds dictionaries from sequences of key:value pairs:
+print(dict([('sape', 4139), ('guido', 4127), ('jack', 4098)]))
+# In addition, dict comprehensions can be used to create dictionaries from arbitrary key and value expressions:
+print({x: x**2 for x in (2, 4, 6)})
+# When the keys are simple strings, it is sometimes easier to specify pairs using keyword arguments:
+print(dict(sape=4139, guido=4127, jack=4098))
 print()
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-The Python Tutorial from https://docs.python.org/3.4/tutorial/index.html.
+The Python Tutorial from https://docs.python.org/3.5/tutorial/index.html.
 
 I'm using Python 3.5.1
 
-5.5. Dictionaries
+I am here:  
+[Looping Techniques](https://docs.python.org/3.5/tutorial/datastructures.html#looping-techniques)
